2770511bc223429ab6de6b646dcf5d24f3d8acf3
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-04-27 16:52:40 +0330

    Make user block move with arrow keys

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWidth = 560;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -57,3 +58,23 @@ function drawUser() {
   user.style.left = currentPosition[0] + "px";
   user.style.bottom = currentPosition[1] + "px";
 }
+
+// Move user
+function moveUser(e) {
+  switch (e.key) {
+    case "ArrowLeft":
+      if (currentPosition[0] > 0) {
+        currentPosition[0] -= 10;
+        drawUser();
+      }
+      break;
+    case "ArrowRight":
+      if (currentPosition[0] + blockWidth < boardWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
+      break;
+  }
+}
+
+document.addEventListener("keydown", moveUser);
